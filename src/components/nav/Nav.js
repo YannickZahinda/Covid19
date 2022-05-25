842d97b902569848dec2423411ebc72a88f9fd5e
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-05-26 00:32:33 +0200

    fix all linters problems

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Nav.css';
+
+const Nav = () => (
+  <div>
+    <ul className="links">
+      <li className="title">
+        Covid
+        <span className="evolution">Evolution &nbsp;&nbsp;</span>
+        <span />
+      </li>
+      <li className="link">
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li className="link">
+        <NavLink to="/Info">Info</NavLink>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Nav;
