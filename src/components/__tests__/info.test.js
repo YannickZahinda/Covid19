41c0ce948490ad182f30811ebfc153636e0d7a02
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-05-26 01:15:15 +0200

    fix linters

--- src/components/__tests__/info.test.js
+++ src/components/__tests__/info.test.js
@@ -1,19 +1,19 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { Provider } from "react-redux";
-import { HashRouter as Router } from "react-router-dom";
-import store from "../../app/store";
-import Info from "../info/info";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { HashRouter as Router } from 'react-router-dom';
+import store from '../../app/store';
+import Info from '../info/info';
 
-describe("info Component test", () => {
-  it("Renders Info link", () => {
+describe('info Component test', () => {
+  it('Renders Info link', () => {
     const app = renderer
       .create(
         <Provider store={store}>
           <Router>
             <Info />
           </Router>
-        </Provider>
+        </Provider>,
       )
       .toJSON();
     expect(app).toMatchSnapshot();
--- src/components/__tests__/nav.test.js
+++ src/components/__tests__/nav.test.js
@@ -1,19 +1,19 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { HashRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "../../app/store";
-import Nav from "../nav/Nav";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { HashRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../../app/store';
+import Nav from '../nav/Nav';
 
-describe("Nav Component test", () => {
-  it("Renders Mission Row", () => {
+describe('Nav Component test', () => {
+  it('Renders Mission Row', () => {
     const app = renderer
       .create(
         <Provider store={store}>
           <Router>
             <Nav />
           </Router>
-        </Provider>
+        </Provider>,
       )
       .toJSON();
     expect(app).toMatchSnapshot();
